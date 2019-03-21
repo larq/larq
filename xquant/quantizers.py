02702824e6c7cb0bcd098e4c99a7b9964e704e64
Lukas Geiger <lukas.geiger94@gmail.com>
2019-03-21 17:42:54 +0000

    Correctly serialize quantizers
    This enables keras layers to be saved and reloaded. Quantizers can also be accessed by its string names

--- xquant/quantizers.py
+++ xquant/quantizers.py
@@ -1,13 +1,42 @@
 import tensorflow as tf
+from xquant import utils
 
 
+@utils.register_keras_custom_object
 @tf.custom_gradient
-def sign(x):
+def sign_ste(x):
     def grad(dy):
         return dy
 
     return tf.sign(x), grad
 
 
-def binarize(x):
-    return sign(tf.clip_by_value(x, -1, 1))
+@utils.register_keras_custom_object
+def sign_clip_ste(x):
+    return sign_ste(tf.clip_by_value(x, -1, 1))
+
+
+def serialize(initializer):
+    return tf.keras.utils.serialize_keras_object(initializer)
+
+
+def deserialize(name, custom_objects=None):
+    return tf.keras.utils.deserialize_keras_object(
+        name,
+        module_objects=globals(),
+        custom_objects=custom_objects,
+        printable_module_name="quantization function",
+    )
+
+
+def get(identifier):
+    if identifier is None:
+        return None
+    if isinstance(identifier, str):
+        return deserialize(str(identifier))
+    elif callable(identifier):
+        return identifier
+    else:
+        raise ValueError(
+            "Could not interpret quantization function identifier:", identifier
+        )
