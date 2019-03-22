c749754910214b03495c6c7040e18bd45c0934cc
Lukas Geiger <lukas.geiger94@gmail.com>
2019-03-22 10:02:18 +0000

    Switch to .sh to see if it fixes CI

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-set -v  # print commands as they're executed
+set -ev  # print commands as they're executed and immediately fail if one has a non zero exit code
 
 pip install tensorflow==$TF_VERSION pytest
 
