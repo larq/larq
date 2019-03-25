7d6afe6ce8ab40d87bd3a9622f6326b8ec54da25
Lukas Geiger <lukas.geiger94@gmail.com>
2019-03-25 12:13:34 +0000

    Move hard_tanh to activations
    This reduces the API surface of the `quantizers` module.

---
+++ xquant/activations.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from xquant import utils
+
+
+@utils.register_keras_custom_object
+def hard_tanh(x):
+    r"""Hard tanh activation function.
+    \\[\sigma(x) = \mathrm{Clip}(x, −1, 1)\\]
+
+    # Arguments
+    x: Input tensor.
+
+    # Returns
+    Hard tanh activation.
+  """
+    return tf.clip_by_value(x, -1, 1)
