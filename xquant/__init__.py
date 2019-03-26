18b4432c5a35f7b4e5d7fe2a0c330e87a02795ca
Lukas Geiger <lukas.geiger94@gmail.com>
2019-03-26 16:57:27 +0000

    Add WeightClip constraint
    Closes #29

--- xquant/__init__.py
+++ xquant/__init__.py
@@ -1,5 +1,6 @@
 import xquant.layers as layers
 import xquant.activations as activations
+import xquant.constraints as constraints
 import xquant.quantizers as quantizers
 
-__all__ = ["layers", "activations", "quantizers"]
+__all__ = ["layers", "activations", "constraints", "quantizers"]
