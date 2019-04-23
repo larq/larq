5b3fff837eea19c671d950b2007cf2d5c445e123
koenhelwegen <kg.helwegen@gmail.com>
2019-04-23 13:59:05 +0100

    Implemented leaky tanh with test (#78)
    * Implemented leaky tanh with test
    
    * implemented merge feedback
    
    * Run black

--- larq/activations.py
+++ larq/activations.py
@@ -38,3 +38,22 @@ def hard_tanh(x):
     Hard tanh activation.
     """
     return tf.clip_by_value(x, -1, 1)
+
+
+@utils.register_keras_custom_object
+def leaky_tanh(x, alpha=0.2):
+    r"""Leaky tanh activation function.
+    Similar to hard tanh, but with non-zero slopes as in leaky ReLU.
+
+    # Arguments
+    x: Input tensor.
+    alpha: Slope of the activation function outside of [-1, 1].
+
+    # Returns
+    Leaky tanh activation.
+    """
+    return (
+        tf.clip_by_value(x, -1, 1)
+        + (tf.math.maximum(x, 1) - 1) * alpha
+        + (tf.math.minimum(x, -1) + 1) * alpha
+    )
--- larq/activations_test.py
+++ larq/activations_test.py
@@ -4,7 +4,7 @@ import pytest
 import larq as lq
 
 
-@pytest.mark.parametrize("name", ["hard_tanh"])
+@pytest.mark.parametrize("name", ["hard_tanh", "leaky_tanh"])
 def test_serialization(name):
     fn = tf.keras.activations.get(name)
     ref_fn = getattr(lq.activations, name)
@@ -20,3 +20,20 @@ def test_hard_tanh():
     f = tf.keras.backend.function([x], [lq.activations.hard_tanh(x)])
     result = f([real_values])[0]
     np.testing.assert_allclose(result, np.clip(real_values, -1, 1))
+
+
+def test_leaky_tanh():
+    @np.vectorize
+    def leaky_tanh(x):
+        if x <= -1:
+            return -1 + 0.2 * (x + 1)
+        elif x <= 1:
+            return x
+        else:
+            return 1 + 0.2 * (x - 1)
+
+    real_values = np.random.uniform(-2, 2, (3, 3, 32))
+    x = tf.keras.backend.placeholder(ndim=2)
+    f = tf.keras.backend.function([x], [lq.activations.leaky_tanh(x)])
+    result = f([real_values])[0]
+    np.testing.assert_allclose(result, leaky_tanh(real_values))
