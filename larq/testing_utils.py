a41ab0e25e43795088d405ed7deec640767806c4
Lukas Geiger <lukas.geiger94@gmail.com>
2019-06-25 23:52:07 +0100

    Screw this, TF 1.13 + eager + keras is broken! Skip this test.
    If this doesn't work, I will drop suport for TF 1.13 as well...

--- larq/testing_utils.py
+++ larq/testing_utils.py
@@ -150,12 +150,14 @@ def layer_test(
     # See b/120160788 for more details. This should be mitigated after 2.0.
     model = tf.keras.models.Model(x, layer(x))
     if _thread_local_data.run_eagerly is not None:
-        # Use tf.train.Optimizer in eager mode for legacy TensorFlow versions
-        # due to compatibility issues
-        opt = "rmsprop" if utils.tf_1_14_or_newer() else tf.train.RMSPropOptimizer(0.01)
-        model.compile(
-            opt, "mse", weighted_metrics=["acc"], run_eagerly=should_run_eagerly()
-        )
+        # TensorFlow < 1.14 + eager is broken, skip the test
+        if utils.tf_1_14_or_newer():
+            model.compile(
+                "rmsprop",
+                "mse",
+                weighted_metrics=["acc"],
+                run_eagerly=should_run_eagerly(),
+            )
     else:
         model.compile("rmsprop", "mse", weighted_metrics=["acc"])
     model.train_on_batch(input_data, actual_output)
