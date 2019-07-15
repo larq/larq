42beb49402ba2e4b833de79053c2b89107136793
MariaHeuss <52407215+MariaHeuss@users.noreply.github.com>
2019-07-15 14:52:24 +0100

    Correct inequality sign in SteTern documentation (#144)
    * Correct inequality sign in SteTern documentation
    
    Changed previously wrong direction of one inequality symbol in SteTern documentation
    
    * Corrected sign in SterTern
    
    Co-Authored-By: Lukas Geiger <lgeiger@users.noreply.github.com>

--- larq/quantizers.py
+++ larq/quantizers.py
@@ -136,8 +136,8 @@ class SteTern:
     \\[
     q(x) = \begin{cases}
     +1 & x > \Delta \\\
-    0 & |x| > \Delta \\\
-    -1 & x < \Delta
+    0 & |x| < \Delta \\\
+     -1 & x < - \Delta
     \end{cases}
     \\]
 
