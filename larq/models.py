4dbc0f8ec9b69305c270289bf512cdf7e01f796b
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2019-08-30 18:02:12 +0100

    Format numbers in larq model summaries to be more readable. Closes #192. (#193)
    * Format numbers in larq model summaries to be more readable. Closes #192.
    
    * Change to three significant figures.
    
    * Use f-strings.

--- larq/models.py
+++ larq/models.py
@@ -48,6 +48,34 @@ def _bitsize_as_str(bitsize):
         raise NotImplementedError()
 
 
+def _number_as_readable_str(num):
+    # The initial rounding here is necessary so that e.g. `999000` gets
+    # formatted as `1.000 M` rather than `1000 k`
+    num = float(f"{num:.3g}")
+
+    # For numbers less than 1000, output them directly, stripping any trailing
+    # zeros and decimal places.
+    if num < 1000:
+        return str(num).rstrip("0").rstrip(".")
+
+    # For numbers that are at least 1000 trillion (1 quadrillion) format with
+    # scientific notation (3 s.f. = 2 d.p. in scientific notation).
+    if num >= 1e15:
+        return f"{num:#.2E}"
+
+    # Count the magnitude.
+    magnitude = 0
+    while abs(num) >= 1000 and magnitude < 4:
+        magnitude += 1
+        num /= 1000.0
+
+    # ':#.3g' formats the number with 3 significant figures, without stripping
+    # trailing zeros.
+    num = f"{num:#.3g}".rstrip(".")
+    unit = ["", " k", " M", " B", " T"][magnitude]
+    return num + unit
+
+
 def _format_table_entry(x, units=1):
     try:
         assert not np.isnan(x)
@@ -295,9 +323,15 @@ class ModelProfile(LayerProfile):
 
     def generate_summary(self, include_macs=True):
         summary = [
-            ["Total params", self.weight_count()],
-            ["Trainable params", self.weight_count(trainable=True)],
-            ["Non-trainable params", self.weight_count(trainable=False)],
+            ["Total params", _number_as_readable_str(self.weight_count())],
+            [
+                "Trainable params",
+                _number_as_readable_str(self.weight_count(trainable=True)),
+            ],
+            [
+                "Non-trainable params",
+                _number_as_readable_str(self.weight_count(trainable=False)),
+            ],
             ["Model size:", f"{self.memory / (8*1024*1024):.2f} MB"],
             [
                 "Float-32 Equivalent",
@@ -309,7 +343,12 @@ class ModelProfile(LayerProfile):
         if include_macs:
             binarization_ratio = self.op_count("mac", 1) / self.op_count(op_type="mac")
             ternarization_ratio = self.op_count("mac", 2) / self.op_count(op_type="mac")
-            summary.append(["Number of MACs", self.op_count(op_type="mac")])
+            summary.append(
+                [
+                    "Number of MACs",
+                    _number_as_readable_str(self.op_count(op_type="mac")),
+                ]
+            )
             if binarization_ratio > 0:
                 summary.append(
                     ["Ratio of MACs that are binarized", f"{binarization_ratio:.4f}"]
