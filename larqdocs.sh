9b10fddbc2e704bbe150b1c336a22bc1ba51ab38
Lukas Geiger <lukas.geiger94@gmail.com>
2019-09-02 11:11:57 +0100

    Move larq_zoo docs to main repo
    Adding documentation for a new larq zoo model can be a bit cumbersome because its API docs have to be manually added and there is no good way to preview the changes to the website.
    This moves the larq_zoo docs into the main repo to allow for easier developement. Also API docs are now automatically generated using the same process as larq it self.
    While this might

--- larqdocs.sh
+++ larqdocs.sh
@@ -3,22 +3,8 @@
 set -e
 basedir=`dirname $0`
 
-# Clean generated output
-rm -r $basedir/docs/models || true
-rm -r $basedir/docs/api || true
-
-mkdir $basedir/docs/models
-
 # Generad API docs
 python $basedir/generate_api_docs.py
-
-# Fetch larq-zoo docs
-curl -o $basedir/docs/models/index.md https://raw.githubusercontent.com/larq/zoo/master/docs/index.md
-curl -o $basedir/docs/models/examples.md https://raw.githubusercontent.com/larq/zoo/master/docs/examples.md
-curl -o $basedir/docs/models/api.md https://raw.githubusercontent.com/larq/zoo/master/docs/api.md
-
-# Fetch larq-zoo plots
-curl -o $basedir/docs/plots/xnornet.vg.json https://raw.githubusercontent.com/larq/zoo/master/docs/plots/xnornet.vg.json
 
 # run mkdocs
 mkdocs $1
