31bfbd50ecccd43900bc5f5ed0bf6abeeedaca56
Maria Heuss <maria@plumerai.com>
2019-09-16 14:52:56 +0200

    Test gradient heaviside

--- larq/quantizers_test.py
+++ larq/quantizers_test.py
@@ -126,18 +126,21 @@ def test_ternarization_with_ternary_weight_networks():
     assert not np.any(result < -1)
 
 
+@pytest.mark.parametrize("fn", [lq.quantizers.ste_sign, lq.quantizers.ste_tern, lq.quantizers.ste_heaviside])
 @pytest.mark.skipif(not tf.executing_eagerly(), reason="requires eager execution")
-def test_ste_grad():
+def test_ste_grad(fn):
     @np.vectorize
     def ste_grad(x):
         if np.abs(x) <= 1:
             return 1.0
         return 0.0
 
+    x = tf.keras.backend.placeholder(ndim=2)
+    f = tf.keras.backend.function([x], [fn(x)])
     x = np.random.uniform(-2, 2, (8, 3, 3, 16))
     tf_x = tf.Variable(x)
     with tf.GradientTape() as tape:
-        activation = lq.quantizers.ste_sign(tf_x)
+        activation = f(tf_x)
     grad = tape.gradient(activation, tf_x)
     np.testing.assert_allclose(grad.numpy(), ste_grad(x))
 
