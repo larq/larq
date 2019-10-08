85f4942ba5d8b1bc05ad9c8dde838dbf1604297d
leonoverweel <l.overweel@gmail.com>
2019-10-08 12:17:09 +0200

    Add explanation of scoped metric definition
    Based on explanation by @lgeiger

--- larq/metrics.py
+++ larq/metrics.py
@@ -1,3 +1,10 @@
+"""We add metrics specific to extremely quantized networks using a `scope` rather than
+through the `metrics` parameter of `model.compile()`, where most common metrics reside.
+This is because, to calculate metrics like the `flip_ratio`, we need a layer's kernel or
+activation and not just the `y_true` and `y_pred` that Keras passes to metrics defined 
+in the usual way.
+"""
+
 import tensorflow as tf
 from larq import utils
 import numpy as np
