1cbf94fcf6b54521a102c0b1e53a0155e32c2606
leonoverweel <l.overweel@gmail.com>
2019-11-04 12:26:59 +0100

    Fix __all__ in optimizers.py

--- larq/optimizers.py
+++ larq/optimizers.py
@@ -3,7 +3,7 @@ from larq import utils
 if utils.tf_1_14_or_newer():
     from larq.optimizers_v2 import Bop, BNNOptimizerDuo
 
-    __all__ = ["Bop", "OptimizerGroup"]
+    __all__ = ["Bop", "BNNOptimizerDuo"]
 else:
     from larq.optimizers_v1 import Bop, XavierLearningRateScaling
 
