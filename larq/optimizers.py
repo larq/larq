1b49c96bbc819e05e89b00c134d799fae006d8ab
leonoverweel <l.overweel@gmail.com>
2019-11-06 10:23:06 +0100

    Fix __all__ in optimizers.py

--- larq/optimizers.py
+++ larq/optimizers.py
@@ -3,7 +3,7 @@ from larq import utils
 if utils.tf_1_14_or_newer():
     from larq.optimizers_v2 import Bop, CaseOptimizer
 
-    __all__ = ["Bop", "OptimizerGroup"]
+    __all__ = ["Bop", "CaseOptimizer"]
 else:
     from larq.optimizers_v1 import Bop, XavierLearningRateScaling
 
