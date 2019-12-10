c5099707124961455809536d84d1224c20b8ad58
Lukas Geiger <lukas.geiger94@gmail.com>
2019-12-09 22:11:26 -0800

    Add packaging as explicit test dependency

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
             "black==19.10b0",
             "flake8~=3.7.9",
             "isort~=4.3.21",
+            "packaging~=19.2",
             "pytest>=5.2.4,<5.4.0",
             "pytest-cov~=2.8.1",
             "pytest-xdist~=1.30.0",
