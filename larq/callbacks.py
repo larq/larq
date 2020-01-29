540526dbc94acf9f048ffaba3824d2adc9d47cc8
Jelmer Neeven <19648598+jneeven@users.noreply.github.com>
2020-01-29 17:35:35 +0100

    Backport HyperParamScheduler to v0.8 (#399)

--- larq/callbacks.py
+++ larq/callbacks.py
@@ -1,43 +1,113 @@
-import tensorflow as tf
+from typing import Any, Callable, MutableMapping, Optional
+
+from tensorflow import keras
 
 
-class HyperparameterScheduler(tf.keras.callbacks.Callback):
+class HyperparameterScheduler(keras.callbacks.Callback):
     """Generic hyperparameter scheduler.
-
+    !!! example
+        ```python
+        bop = lq.optimizers.Bop(threshold=1e-6, gamma=1e-3)
+        adam = tf.keras.optimizers.Adam(0.01)
+        optimizer = lq.optimizers.CaseOptimizer(
+            (lq.optimizers.Bop.is_binary_variable, bop), default_optimizer=adam,
+        )
+        callbacks = [
+            HyperparameterScheduler(lambda x: 0.001 * (0.1 ** (x // 30)), "gamma", bop)
+        ]
+        ```
     # Arguments
+    optimizer: the optimizer that contains the hyperparameter that will be scheduled.
+        Defaults to `self.model.optimizer` if `optimizer == None`.
     schedule: a function that takes an epoch index as input
         (integer, indexed from 0) and returns a new hyperparameter as output.
     hyperparameter: str. the name of the hyperparameter to be scheduled.
+    update_freq: str (optional), denotes on what update_freq to change the
+        hyperparameter. Can be either "epoch" (default) or "step".
     verbose: int. 0: quiet, 1: update messages.
+    log_name: str (optional), under which name to log this hyperparameter to
+        Tensorboard. If `None`, defaults to `hyperparameter`. Use this if you have
+        several schedules for the same hyperparameter on different optimizers.
     """
 
-    def __init__(self, schedule, hyperparameter, verbose=0):
+    def __init__(
+        self,
+        schedule: Callable,
+        hyperparameter: str,
+        optimizer: Optional[keras.optimizers.Optimizer] = None,
+        update_freq: str = "epoch",
+        verbose: int = 0,
+        log_name: Optional[str] = None,
+    ):
         super(HyperparameterScheduler, self).__init__()
+        self.optimizer = optimizer
         self.schedule = schedule
         self.hyperparameter = hyperparameter
+        self.log_name = log_name or hyperparameter
         self.verbose = verbose
 
-    def on_epoch_begin(self, epoch, logs=None):
-        if not hasattr(self.model.optimizer, self.hyperparameter):
+        if update_freq not in ["epoch", "step"]:
+            raise ValueError(
+                "HyperparameterScheduler.update_freq can only be 'step' or 'epoch'."
+                f" Received value '{update_freq}'"
+            )
+
+        self.update_freq = update_freq
+
+    def set_model(self, model: keras.models.Model) -> None:
+        super().set_model(model)
+        if self.optimizer is None:
+            # It is not possible for a model to reach this state and not have
+            # an optimizer, so we can safely access it here.
+            self.optimizer = model.optimizer
+
+        if not hasattr(self.optimizer, self.hyperparameter):
             raise ValueError(
                 f'Optimizer must have a "{self.hyperparameter}" attribute.'
             )
 
-        hp = getattr(self.model.optimizer, self.hyperparameter)
+    def set_hyperparameter(self, t: int) -> Any:
+        hp = getattr(self.optimizer, self.hyperparameter)
         try:  # new API
-            hyperparameter_val = tf.keras.backend.get_value(hp)
-            hyperparameter_val = self.schedule(epoch, hyperparameter_val)
+            hyperparameter_val = keras.backend.get_value(hp)
+            hyperparameter_val = self.schedule(t, hyperparameter_val)
         except TypeError:  # Support for old API for backward compatibility
-            hyperparameter_val = self.schedule(epoch)
+            hyperparameter_val = self.schedule(t)
+        keras.backend.set_value(hp, hyperparameter_val)
+        return hp
 
-        tf.keras.backend.set_value(hp, hyperparameter_val)
+    def on_batch_begin(
+        self, batch: int, logs: Optional[MutableMapping[str, Any]] = None
+    ) -> None:
+        if not self.update_freq == "step":
+            return
+
+        # We use optimizer.iterations (i.e. global step), since batch only
+        # reflects the batch index in the current epoch.
+        batch = keras.backend.get_value(self.optimizer.iterations)
+        hp = self.set_hyperparameter(batch)
 
         if self.verbose > 0:
             print(
-                f"Epoch {epoch + 1}: {self.hyperparameter} changning to {tf.keras.backend.get_value(hp)}."
+                f"Batch {batch}: {self.log_name} is now {keras.backend.get_value(hp)}."
             )
 
-    def on_epoch_end(self, epoch, logs=None):
+    def on_epoch_begin(
+        self, epoch: int, logs: Optional[MutableMapping[str, Any]] = None
+    ) -> None:
+        if not self.update_freq == "epoch":
+            return
+
+        hp = self.set_hyperparameter(epoch)
+
+        if self.verbose > 0:
+            print(
+                f"Epoch {epoch}: {self.log_name} is now {keras.backend.get_value(hp)}."
+            )
+
+    def on_epoch_end(
+        self, epoch: int, logs: Optional[MutableMapping[str, Any]] = None
+    ) -> None:
         logs = logs or {}
-        hp = getattr(self.model.optimizer, self.hyperparameter)
-        logs[self.hyperparameter] = tf.keras.backend.get_value(hp)
+        hp = getattr(self.optimizer, self.hyperparameter)
+        logs[self.log_name] = keras.backend.get_value(hp)
--- larq/callbacks_test.py
+++ larq/callbacks_test.py
@@ -1,21 +1,193 @@
+import math
+
+import numpy as np
+import pytest
 import tensorflow as tf
+from tensorflow.python.keras import testing_utils
 
-
-class LogHistory(tf.keras.callbacks.Callback):
-    def on_train_begin(self, logs={}):
-        self.batches = []
-        self.epochs = []
-
-    def _store_logs(self, storage, batch_or_epoch, logs={}):
-        if [key for key in logs if "changed_quantization_ration" in key]:
-            storage.append(batch_or_epoch)
-
-    def on_batch_end(self, batch, logs={}):
-        self._store_logs(self.batches, batch, logs)
-
-    def on_epoch_end(self, epoch, logs={}):
-        self._store_logs(self.epochs, epoch, logs)
+import larq as lq
+from larq import testing_utils as lq_testing_utils
+from larq.callbacks import HyperparameterScheduler
 
 
 class TestHyperparameterScheduler:
-    pass
+    def _create_data_and_model(self, train_samples=1000):
+        np.random.seed(1337)
+        (x_train, y_train), (x_test, y_test) = testing_utils.get_test_data(
+            train_samples=train_samples,
+            test_samples=0,
+            input_shape=(10,),
+            num_classes=2,
+        )
+        y_train = tf.keras.utils.to_categorical(y_train)
+
+        model = lq_testing_utils.get_small_bnn_model(
+            x_train.shape[1], 20, y_train.shape[1]
+        )
+
+        return x_train, y_train, model
+
+    def test_normal_optimizer(self):
+        x_train, y_train, model = self._create_data_and_model()
+
+        model.compile(
+            loss="categorical_crossentropy",
+            optimizer=tf.keras.optimizers.Adam(0.01),
+            metrics=["accuracy"],
+        )
+
+        def scheduler(x):
+            return 1.0 / (1.0 + x)
+
+        # We shouldn' t need to specify the optimizer
+        test_scheduler = HyperparameterScheduler(
+            schedule=scheduler, hyperparameter="lr", verbose=1,
+        )
+
+        num_epochs = 2
+        model.fit(
+            x_train,
+            y_train,
+            epochs=num_epochs,
+            batch_size=16,
+            callbacks=[test_scheduler],
+            verbose=0,
+        )
+
+        np.testing.assert_almost_equal(
+            tf.keras.backend.get_value(model.optimizer.lr),
+            scheduler(num_epochs - 1),
+            decimal=8,
+        )
+
+    def test_per_step(self):
+        train_samples = 20
+        x_train, y_train, model = self._create_data_and_model(train_samples)
+
+        model.compile(
+            loss="categorical_crossentropy",
+            optimizer=tf.keras.optimizers.Adam(0.01),
+            metrics=["accuracy"],
+        )
+
+        def scheduler(x):
+            return 1.0 / (1.0 + x)
+
+        # Test that we don't accept incorrect `update_freq`
+        with pytest.raises(ValueError):
+            HyperparameterScheduler(
+                schedule=scheduler, hyperparameter="lr", update_freq="wrong",
+            )
+
+        # The actual scheduler we'll use
+        test_scheduler = HyperparameterScheduler(
+            schedule=scheduler, hyperparameter="lr", update_freq="step", verbose=1,
+        )
+
+        num_epochs = 1
+        batch_size = 10
+        model.fit(
+            x_train,
+            y_train,
+            epochs=num_epochs,
+            batch_size=16,
+            callbacks=[test_scheduler],
+            verbose=0,
+        )
+
+        np.testing.assert_almost_equal(
+            tf.keras.backend.get_value(model.optimizer.lr),
+            scheduler(math.ceil(train_samples / batch_size) - 1),
+            decimal=8,
+        )
+
+    def test_case_optimizer(self):
+        x_train, y_train, model = self._create_data_and_model()
+
+        bop = lq.optimizers.Bop(threshold=1e-6, gamma=1e-3)
+        adam = tf.keras.optimizers.Adam(0.01)
+        case_optimizer = lq.optimizers.CaseOptimizer(
+            (lq.optimizers.Bop.is_binary_variable, bop), default_optimizer=adam,
+        )
+
+        model.compile(
+            loss="categorical_crossentropy",
+            optimizer=case_optimizer,
+            metrics=["accuracy"],
+        )
+
+        def scheduler(x):
+            return 1.0 / (1.0 + x)
+
+        cbk_gamma_scheduler = HyperparameterScheduler(
+            schedule=scheduler,
+            optimizer=model.optimizer.optimizers[0],
+            hyperparameter="gamma",
+            verbose=1,
+        )
+        cbk_threshold_scheduler = HyperparameterScheduler(
+            schedule=scheduler,
+            optimizer=model.optimizer.optimizers[0],
+            hyperparameter="threshold",
+            verbose=1,
+        )
+        cbk_lr_scheduler = HyperparameterScheduler(
+            schedule=scheduler,
+            optimizer=model.optimizer.optimizers[1],
+            hyperparameter="lr",
+            verbose=1,
+        )
+
+        num_epochs = 3
+        model.fit(
+            x_train,
+            y_train,
+            epochs=num_epochs,
+            batch_size=16,
+            callbacks=[cbk_gamma_scheduler, cbk_lr_scheduler, cbk_threshold_scheduler],
+            verbose=0,
+        )
+
+        np.testing.assert_almost_equal(
+            tf.keras.backend.get_value(model.optimizer.optimizers[0].gamma),
+            scheduler(num_epochs - 1),
+            decimal=8,
+        )
+
+        np.testing.assert_almost_equal(
+            tf.keras.backend.get_value(model.optimizer.optimizers[0].threshold),
+            scheduler(num_epochs - 1),
+            decimal=8,
+        )
+
+        np.testing.assert_almost_equal(
+            tf.keras.backend.get_value(model.optimizer.optimizers[1].lr),
+            scheduler(num_epochs - 1),
+            decimal=8,
+        )
+
+    def test_wrong_param(self):
+        x_train, y_train, model = self._create_data_and_model()
+
+        model.compile(
+            loss="categorical_crossentropy",
+            optimizer=tf.keras.optimizers.Adam(0.01),
+            metrics=["accuracy"],
+        )
+
+        def scheduler(x):
+            return 1.0 / (1.0 + x)
+
+        wrong_scheduler = HyperparameterScheduler(
+            schedule=scheduler, hyperparameter="invalid_param", verbose=1,
+        )
+
+        with pytest.raises(ValueError):
+            model.fit(
+                x_train,
+                y_train,
+                epochs=1,
+                batch_size=16,
+                callbacks=[wrong_scheduler],
+                verbose=0,
+            )
