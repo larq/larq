fe056bbdabfa35ccb475485d7731ac8afe5314a0
Lukas Geiger <lukas.geiger94@gmail.com>
2020-02-17 13:54:35 +0000

    Add LCE docs

---
+++ download_lce_docs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+basedir=`dirname $0`
+
+# Fetch compute-engine docs
+echo "# Larq Compute Engine" > $basedir/docs/compute-engine/index.md
+curl https://raw.githubusercontent.com/larq/compute-engine/master/README.md | tail -n +4 >> $basedir/docs/compute-engine/index.md
+for file in build.md build_arm.md inference.md quickstart_android.md converter.md
+do
+  curl https://raw.githubusercontent.com/larq/compute-engine/master/docs/$file > $basedir/docs/compute-engine/$file
+done
