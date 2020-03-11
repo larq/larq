4e3a66b5bdca183584b8d6b8fa7ae33c6e47b06e
Lukas Geiger <lgeiger@users.noreply.github.com>
2020-03-11 15:04:35 +0000

    Add quantized variable fixes from TF upstream (#452)
    * Replace experimental_ref with ref
    
    See https://github.com/tensorflow/tensorflow/commit/9c7c7c9979d66e97aeb8717d87f351df5ecbf56b#diff-8db8dd75efb914430be51c632dfa452e
    
    * Support QuantizedVariable in CentralStorageStrategy
    
    See https://github.com/tensorflow/tensorflow/commit/d4dacbcb2a9a58acaee6c7c30966281cb7f35073#diff-8db8dd75efb914430be51c632dfa452e
    
    * Support restoring SavedModels in graph mode
    
    See https://github.com/tensorflow/tensorflow/commit/1fcda57f37c2ac854cabf1c3462eb14e39d36c60
    
    * Silence pytype
    
    * Make isort happy and me sad

--- larq/quantized_variable.py
+++ larq/quantized_variable.py
@@ -3,7 +3,10 @@
 from typing import Optional
 
 import tensorflow as tf
-from tensorflow.python.distribute.values import DistributedVariable
+from tensorflow.python.distribute.values import (  # type: ignore
+    AggregatingVariable,
+    DistributedVariable,
+)
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import resource_variable_ops
 
@@ -74,11 +77,15 @@ class QuantizedVariable(tf.Variable):
         # Returns
         A QuantizedVariable that wraps the variable.
         """
-        if not isinstance(variable, DistributedVariable):  # type: ignore
+        if not isinstance(variable, (DistributedVariable, AggregatingVariable)):  # type: ignore
             return cls(variable, quantizer, precision)
 
         class QuantizedDistributedVariable(cls, variable.__class__):
-            """A QuantizedVariable that also subclasses from DistributedVariable."""
+            """A QuantizedVariable that also subclasses from `variable.__class__`.
+
+            `variable.__class__` is either a `DistributedVariable` or an
+            `AggregatingVariable`.
+            """
 
             def get(self, *args, **kwargs):
                 # For some reason this is needed to make unit `x + x` pass on TF 1.14
@@ -184,7 +191,7 @@ class QuantizedVariable(tf.Variable):
     #
     # We do not define the following methods from Variable for the following
     # reasons:
-    #   * 'experimental_ref': Instead we inherit the definition from Variable.
+    #   * 'ref': Instead we inherit the definition from Variable.
     #     If we defined and delegated to Variable, the ref of an QuantizedVariable
     #     would be the same as the ref of the underlying variable, which would be
     #     strange as they are different Python objects.
@@ -318,6 +325,31 @@ class QuantizedVariable(tf.Variable):
 
     def from_proto(self, *args, **kwargs):
         return self.latent_variable.from_proto(*args, **kwargs)
+
+    # Delegate the private attributes _handle_name and _initializer_op to
+    # self._variable. SavedModel sets these attributes when loading a model. For
+    # example, it sets _handle_name here:
+    # https://github.com/tensorflow/tensorflow/blob/db26bd574fa95b5bdd53c08463dd19407cc0297e/tensorflow/python/keras/saving/saved_model/load.py#L211
+    # We need to expose these attributes on AutoCastVariable as well for
+    # SavedModel to work properly.
+    # TODO: Find a better way to support SavedModel. Exposing private attributes is
+    # hacky and difficult to maintain.
+    # For more info see https://github.com/tensorflow/tensorflow/commit/1fcda57f37c2ac854cabf1c3462eb14e39d36c60
+    @property
+    def _handle_name(self):
+        return self._variable._handle_name
+
+    @_handle_name.setter
+    def _handle_name(self, handle_name):
+        self._variable._handle_name = handle_name
+
+    @property
+    def _initializer_op(self):
+        return self._variable._initializer_op
+
+    @_initializer_op.setter
+    def _initializer_op(self, initializer_op):
+        self._variable._initializer_op = initializer_op
 
     def _as_graph_element(self):
         return self._quantize(self.latent_variable._as_graph_element())
