dee40e36ac8486f0a38361b161db84c7e68f0af6
koenhelwegen <koen@plumerai.com>
2020-04-27 09:29:44 +0100

    prevent op_count=0

--- larq/models.py
+++ larq/models.py
@@ -190,7 +190,10 @@ class LayerProfile:
         if op_type != "mac":
             raise ValueError("Currently only counting of MAC-operations is supported.")
 
-        if isinstance(self._layer, op_count_supported_layer_types):
+        if (
+            isinstance(self._layer, op_count_supported_layer_types)
+            and self.output_pixels
+        ):
             count = 0
             for op in self.op_profiles:
                 if (precision is None or op.precision == precision) and (
