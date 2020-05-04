17eb91aa32727655b4ae7c2d49b98712f8938a1f
leonoverweel <l.overweel@gmail.com>
2020-05-04 14:24:54 +0200

    Refactor summary() to no longer pass print as arg

--- larq/models.py
+++ larq/models.py
@@ -423,7 +423,7 @@ class SummaryTable(AsciiTable):
 
 
 def summary(
-    model, print_fn: Callable[[str], Any] = print, include_macs: bool = True
+    model, print_fn: Callable[[str], Any] = None, include_macs: bool = True
 ) -> None:
     """Prints a string summary of the network.
 
@@ -466,6 +466,9 @@ def summary(
             "`model.build()` or calling `model.fit()` with some data, or specify an "
             "`input_shape` argument in the first layer(s) for automatic build."
         )
+
+    if not print_fn:
+        print_fn = print
 
     model_profile = ModelProfile(model)
     print_fn(
