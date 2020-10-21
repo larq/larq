067f955e3698e7cbac6d87ec99667a4bd27e8aca
Adam Hillier <7688302+AdamHillier@users.noreply.github.com>
2020-10-21 18:13:40 +0100

    Add __version__ property. Closes #595.

--- larq/__init__.py
+++ larq/__init__.py
@@ -12,6 +12,14 @@ from larq import (
     utils,
 )
 
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
+
+__version__ = metadata.version("larq")
+
 __all__ = [
     "layers",
     "activations",
