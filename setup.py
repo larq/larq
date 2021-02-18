d75115767ae4a5c3a89f360fddac510db0e0fdce
Lukas Geiger <lukas.geiger94@gmail.com>
2021-02-19 00:34:52 +0100

    :arrow_up: 0.11.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="larq",
-    version="0.11.1",
+    version="0.11.2",
     python_requires=">=3.6",
     author="Plumerai",
     author_email="opensource@plumerai.com",
