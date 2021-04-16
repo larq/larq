6215854f3c4b35f9490723d78fc9b22fc75b3c4b
Joschua Conrad <joschua.conrad@uni-ulm.de>
2021-04-16 08:14:38 +0200

    Added test for metrics of DoReFaKernel quantizer

--- larq/quantizers_test.py
+++ larq/quantizers_test.py
@@ -350,6 +350,7 @@ class TestGradients:
         ("magnitude_aware_sign", lq.quantizers.MagnitudeAwareSign),
         ("ste_tern", lq.quantizers.SteTern),
         ("dorefa_quantizer", lq.quantizers.DoReFa),
+		("dorefa_kernel_quantizer", lq.quantizers.DoReFaKernel),
     ],
 )
 def test_metrics(quantizer):
